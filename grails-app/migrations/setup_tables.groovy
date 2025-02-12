bb51c7a7d78e53ad3dfe39c2736538bd06a30c96
kierpagdato <kier.p@ondeviceresearch.com>
2025-02-12 14:50:35 +0800

    Change table creation

--- grails-app/migrations/setup_tables.groovy
+++ grails-app/migrations/setup_tables.groovy
@@ -1,31 +1,16 @@
 databaseChangeLog = {
-    changeSet(author: "Kier", id: "1111111111111-1") {
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-1") {
         createTable(tableName: "book") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(nullable: "false", primaryKey: "true", primaryKeyName: "bookPK")
             }
 
-            column(name: "title", type: "VARCHAR(50)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "author", type: "VARCHAR(50)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "isbn", type: "VARCHAR(50)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "status", type: "VARCHAR(50)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "description", type: "text") {
-                constraints(nullable: "false")
-            }
-
             column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "title", type: "VARCHAR(50)") {
                 constraints(nullable: "false")
             }
 
@@ -36,24 +21,32 @@ databaseChangeLog = {
             column(name: "last_updated", type: "datetime") {
                 constraints(nullable: "false")
             }
-        }
-    } // end create table book
 
-    changeSet(author: "Kier", id: "1111111111111-2") {
+            column(name: "isbn", type: "VARCHAR(50)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "status", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "description", type: "CLOB") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "author", type: "VARCHAR(50)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-2") {
         createTable(tableName: "borrow") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(nullable: "false", primaryKey: "true", primaryKeyName: "borrowPK")
             }
 
-            column(name: "date_borrowed", type: "datetime") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "date_returned", type: "datetime") {
-                constraints(nullable: "true")
-            }
-
-            column(name: "book_id", type: "BIGINT") {
+            column(name: "version", type: "BIGINT") {
                 constraints(nullable: "false")
             }
 
@@ -65,80 +58,36 @@ databaseChangeLog = {
                 constraints(nullable: "false")
             }
 
-            column(name: "version", type: "BIGINT") {
+            column(name: "date_borrowed", type: "datetime") {
                 constraints(nullable: "false")
             }
 
-            column(name: "date_created", type: "datetime") {
+            column(name: "date_returned", type: "datetime") {
                 constraints(nullable: "false")
             }
 
-            column(name: "last_updated", type: "datetime") {
-                constraints(nullable: "false")
-            }
-        }
-    } // end create table borrow
-
-    changeSet(author: "Kier", id: "1111111111111-3") {
-        createTable(tableName: "user") {
-            column(autoIncrement: "true", name: "id", type: "BIGINT") {
-                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "userPK")
-            }
-
-            column(name: "username", type: "VARCHAR(255)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "password", type: "VARCHAR(255)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "enabled", type: "BOOLEAN") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "account_expired", type: "BOOLEAN") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "email", type: "VARCHAR(255)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "first_name", type: "VARCHAR(255)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "last_name", type: "VARCHAR(255)") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "account_locked", type: "BOOLEAN") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "password_expired", type: "BOOLEAN") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "version", type: "BIGINT") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "date_created", type: "datetime") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "last_updated", type: "datetime") {
+            column(name: "book_id", type: "BIGINT") {
                 constraints(nullable: "false")
             }
         }
-    } // end create table user
+    }
 
-    changeSet(author: "Kier", id: "1111111111111-4") {
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-3") {
         createTable(tableName: "role") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(nullable: "false", primaryKey: "true", primaryKeyName: "rolePK")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "date_created", type: "datetime") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "last_updated", type: "datetime") {
+                constraints(nullable: "false")
             }
 
             column(name: "authority", type: "VARCHAR(255)") {
@@ -148,6 +97,14 @@ databaseChangeLog = {
             column(name: "text", type: "VARCHAR(255)") {
                 constraints(nullable: "false")
             }
+        }
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-4") {
+        createTable(tableName: "user") {
+            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "userPK")
+            }
 
             column(name: "version", type: "BIGINT") {
                 constraints(nullable: "false")
@@ -157,13 +114,49 @@ databaseChangeLog = {
                 constraints(nullable: "false")
             }
 
+            column(name: "first_name", type: "VARCHAR(50)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "password_expired", type: "BOOLEAN") {
+                constraints(nullable: "false")
+            }
+
             column(name: "last_updated", type: "datetime") {
                 constraints(nullable: "false")
             }
-        }
-    } // end create table role
 
-    changeSet(author: "Kier", id: "1111111111111-5") {
+            column(name: "account_expired", type: "BOOLEAN") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "username", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "account_locked", type: "BOOLEAN") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "password", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "last_name", type: "VARCHAR(50)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "enabled", type: "BOOLEAN") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "email", type: "VARCHAR(50)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-5") {
         createTable(tableName: "user_role") {
             column(name: "user_id", type: "BIGINT") {
                 constraints(nullable: "false", primaryKey: "true", primaryKeyName: "user_rolePK")
@@ -173,5 +166,33 @@ databaseChangeLog = {
                 constraints(nullable: "false", primaryKey: "true", primaryKeyName: "user_rolePK")
             }
         }
-    } // end create table user role
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-6") {
+        addUniqueConstraint(columnNames: "authority", constraintName: "UC_ROLEAUTHORITY_COL", tableName: "role")
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-7") {
+        addUniqueConstraint(columnNames: "username", constraintName: "UC_USERUSERNAME_COL", tableName: "user")
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-8") {
+        addForeignKeyConstraint(baseColumnNames: "borrower_id", baseTableName: "borrow", constraintName: "FK5745ehusnigepril8saky3e07", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "user", validate: "true")
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-9") {
+        addForeignKeyConstraint(baseColumnNames: "user_id", baseTableName: "user_role", constraintName: "FK859n2jvi8ivhui0rl0esws6o", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "user", validate: "true")
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-10") {
+        addForeignKeyConstraint(baseColumnNames: "role_id", baseTableName: "user_role", constraintName: "FKa68196081fvovjhkek5m97n3y", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "role", validate: "true")
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-11") {
+        addForeignKeyConstraint(baseColumnNames: "book_id", baseTableName: "borrow", constraintName: "FKgqh01ty3c1u7ja2rjdua05c36", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "book", validate: "true")
+    }
+
+    changeSet(author: "kierpagdato (generated)", id: "1739336681435-12") {
+        addForeignKeyConstraint(baseColumnNames: "processor_id", baseTableName: "borrow", constraintName: "FKhbaf4k5w6nj6cmcyminl9a4u8", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "user", validate: "true")
+    }
 }
